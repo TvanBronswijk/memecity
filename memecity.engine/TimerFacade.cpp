7847e34f681d43b7ffc5819a0e21296aaed56d74
Simon Heij <simon.heij@gmail.com>
2018-10-01 10:22:23 +0200

    Added TimerFacade.

--- memecity.engine/TimerFacade.cpp
+++ memecity.engine/TimerFacade.cpp
@@ -1,1 +1,58 @@
 ﻿#include "TimerFacade.h"
+#include <SDL.h>
+
+TimerFacade* TimerFacade::instance = nullptr;
+
+TimerFacade* TimerFacade::GetInstance()
+{
+	if (instance == nullptr)
+		instance = new TimerFacade();
+
+	return instance;
+}
+
+void TimerFacade::Release()
+{
+	delete instance;
+	instance = nullptr;
+}
+
+TimerFacade::TimerFacade()
+{
+	Reset();
+	timeScale = 1.0f;
+}
+
+TimerFacade::~TimerFacade()
+{
+
+}
+
+void TimerFacade::Reset()
+{
+	startTicks = SDL_GetTicks();
+	elaspsedTicks = 0;
+	deltaTime = 0.0f;
+}
+
+float TimerFacade::DeltaTime()
+{
+	return deltaTime;
+}
+
+void TimerFacade::TimeScale(float t)
+{
+	timeScale = t;
+}
+
+
+float TimerFacade::TimeScale()
+{
+	return timeScale;
+}
+
+void TimerFacade::Update()
+{
+	elaspsedTicks = SDL_GetTicks() - startTicks;
+	deltaTime = elaspsedTicks * 0.001f;
+}
--- memecity.engine/TimerFacade.h
+++ memecity.engine/TimerFacade.h
@@ -1,10 +1,33 @@
 ﻿#ifndef _TIMERFACADE_H
 #define _TIMERFACADE_H
+#include <SDL.h>
 
 class TimerFacade
 {
+
+private:
+	static TimerFacade* instance;
+
+	unsigned int startTicks;
+	unsigned int elaspsedTicks;
+	float deltaTime;
+	float timeScale;
+
 public:
-	
+	static TimerFacade* GetInstance();
+	static void Release();
+
+	void Reset();
+	float DeltaTime();
+
+	void TimeScale(float t);
+	float TimeScale();
+
+	void Update();
+
+private:
+	TimerFacade();
+	~TimerFacade();
 };
 
 #endif
