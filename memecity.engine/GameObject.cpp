a656dac183e5ab2b5bb1999318c55c6d61825dfe
Simon Heij <simon.heij@gmail.com>
2018-10-02 14:38:28 +0200

    Added files.

---
+++ memecity.engine/GameObject.cpp
@@ -0,0 +1,88 @@
+#include "GameObject.h"
+
+GameObject::GameObject(float x, float y)
+{
+	position.x = x;
+	position.y = y;
+
+	rotation = 0.0f;
+	is_active = true;
+	parent = nullptr;
+}
+
+GameObject::~GameObject()
+{
+	parent = nullptr;
+}
+
+void GameObject::set_position(Vector2 position)
+{
+	this->position = position;
+}
+
+Vector2 GameObject::get_position(SPACE space)
+{
+	if (space == local || parent == nullptr)
+		return position;
+
+	return parent->get_position(world) + RotateVector(position, parent->get_rotation(local));
+}
+void GameObject::set_rotation(float rotation)
+{
+	rotation = rotation;
+
+	if (rotation > 360.0f) {
+		int mul = rotation / 360;
+		rotation -= 360.0f * mul;
+	}
+	else if (rotation < 0.0f) {
+		int mul = (rotation / 360) - 1;
+		rotation -= 360.0f * mul;
+	}
+}
+
+float GameObject::get_rotation(SPACE space)
+{
+	if (space == local || parent == nullptr)
+		return rotation;
+
+	return parent->get_rotation(world) + rotation;
+}
+
+void GameObject::set_active(bool active)
+{
+	is_active = active;
+}
+
+bool GameObject::get_active()
+{
+	return is_active;
+}
+
+void GameObject::get_parent(std::shared_ptr<GameObject> parent)
+{
+	position = get_position(world) - parent->get_position(world);
+
+	parent = parent;
+}
+
+std::shared_ptr<GameObject> GameObject::get_parent()
+{
+	return parent;
+}
+
+void GameObject::translate(Vector2 vector)
+{
+	position += vector;
+}
+
+
+void GameObject::update()
+{
+
+}
+
+void GameObject::render()
+{
+
+}
