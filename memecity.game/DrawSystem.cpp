efcb5ee7d3307a00f7f7ea9e6aa3985c775e583a
Simon Heij <sheij1@student.avans.nl>
2018-10-23 15:46:45 +0200

    implemented viewport

--- memecity.game/DrawSystem.cpp
+++ memecity.game/DrawSystem.cpp
@@ -14,19 +14,15 @@ void DrawSystem::run(EntityManager& em)
 	auto player_position_component = dynamic_cast<PositionComponent*>(em.get_component_of_entity(player_components[0]->entity_id, PositionComponent::COMPONENT_TYPE));
 
 	auto drawable_components = em.get_components_of_type(DrawableComponent::COMPONENT_TYPE);
-//	auto position_components = em.get_components_of_type(PositionComponent::COMPONENT_TYPE);
-	//for (auto position_component : position_components)
-	//{
-		for (auto drawable_component : drawable_components)
-		{
-			if (drawable_component->entity_id != player_position_component->entity_id)
-			{
-				auto tex = dynamic_cast<DrawableComponent*>(drawable_component)->texture;
-				tex->translate({ (player_position_component->diffx*-1) , player_position_component->diffy });
-			}
-		}
-	//}
 
+	for (auto drawable_component : drawable_components)
+	{
+		if (drawable_component->entity_id != player_position_component->entity_id)
+		{
+			auto tex = dynamic_cast<DrawableComponent*>(drawable_component)->texture;
+			tex->translate({ (player_position_component->diffx*-1) , player_position_component->diffy });
+		}
+	}
 
 	multimedia_manager.lock()->clear_graphics();
 	for (Component* component : drawable_components)
