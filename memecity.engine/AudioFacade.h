2403c8cae120efe761bb07f06a522be2dcb1cd3a
Rick van Berlo <rickvanberlo@gmail.com>
2018-11-06 11:19:52 +0100

    const int deleted

--- memecity.engine/AudioFacade.h
+++ memecity.engine/AudioFacade.h
@@ -12,16 +12,16 @@ public:
 	bool init() const;
 
 	///<summary>Open the SDL audio mixer with a given audio format.</summary>
-	void open_audio(int frequency,const int channels,const int chunksize) const;
+	void open_audio(int frequency, int channels, int chunksize) const;
 
 	///<summary>Play a sound effect with a given amount of repeats, volume and the selected channel.</summary>
-	void play_sound_effect(Mix_Chunk* sound,const int repeats,const int volume,const int channel = 0) const;
+	void play_sound_effect(Mix_Chunk* sound, int repeats, int volume,  int channel = 0) const;
 
 	///<summary>Checks if audio is playing on the given channel.</summary>
 	bool is_playing(const int channel) const;
 
 	///<summary>Play a given background music with a specified volume.</summary>
-	void play_background_music(Mix_Music* music,const int volume) const;
+	void play_background_music(Mix_Music* music, int volume) const;
 
 	///<summary>If the music is paused, resume. Otherwise pause.</summary>
 	void pause_background_music() const;
