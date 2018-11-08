9d3a5f23ad4a435ef919859d4d54ef163d1312f0
Rick van Berlo <rickvanberlo@gmail.com>
2018-11-08 10:00:13 +0100

    added const to input manager

--- memecity.engine/InputManager.cpp
+++ memecity.engine/InputManager.cpp
@@ -10,12 +10,12 @@ void InputManager::update()
 	input_facade->update();
 }
 
-bool InputManager::is_pressed(InputKeys key)
+bool InputManager::is_pressed(InputKeys key) const
 {
 	return input_facade->is_pressed(key);
 }
 
-bool InputManager::is_quit_pressed()
+bool InputManager::is_quit_pressed() const
 {
 	return input_facade->is_quit_pressed();
 }
