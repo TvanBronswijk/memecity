0a765c51b0d9d471b5aeede2adcf2cfa3c256bbf
Thom van de Pas <thomvandepas@hotmail.com>
2018-11-08 11:01:24 +0100

    Updated the GameManager init() to dev.

--- memecity.game/GameManager.cpp
+++ memecity.game/GameManager.cpp
@@ -16,25 +16,16 @@ void GameManager::init()
 	auto position_component = new PositionComponent(entity, multimedia_manager.get_screen_width() / 2, multimedia_manager.get_screen_height() / 2);
 	entity_manager->register_component(position_component);
 
-		auto d_component = new DrawableComponent(entity);
+	auto d_component = new DrawableComponent(entity);
 
-		animated_character = multimedia_manager->get_animated_texture(timer.get(), "SpriteSheet.png", 0, 0, 48, 48, 4, 0.5f, AnimatedCharacter::vertical);
-		d_component->texture = animated_character;
-		d_component->texture->set_position({ position_component->x, position_component->y });
-		entity_manager->register_component(d_component);
+	animated_character = multimedia_manager.get_animated_texture(timer.get(), "SpriteSheet.png", 0, 0, 48, 48, 4, 0.5f, AnimatedCharacter::vertical);
+	d_component->texture = animated_character;
+	d_component->texture->set_position({ position_component->x, position_component->y });
+	entity_manager->register_component(d_component);
 
 	entity_manager->register_system(new InputSystem(input_manager));
 	entity_manager->register_system(new DrawSystem(multimedia_manager));
 	entity_manager->register_system(new MoveSystem());
-
-	// Test to show an example how to create a animated character
-	animated_character = multimedia_manager.get_animated_texture(timer.get(), "SpriteSheet.png", 0, 0, 48, 48, 4, 0.5f, AnimatedCharacter::vertical);
-	animated_character->set_position(Vector2(200.0, 200.0));
-
-	const auto animated_character_entity = entity_manager->create_entity();
-	const auto drawable_component = new DrawableComponent(animated_character_entity);
-	entity_manager->register_component(drawable_component);
-	drawable_component->texture = animated_character;
 }
 
 void GameManager::handle()
