b804f7072e9eb5977b449f2fcd44eba95c41053d
Rick van Berlo <rickvanberlo@gmail.com>
2018-11-12 15:33:12 +0100

    healthcomponent change 1 to 0

--- memecity.game/HealthComponent.h
+++ memecity.game/HealthComponent.h
@@ -7,7 +7,7 @@ struct HealthComponent : public ecs::Component{
 public:
 	int health;
 
-	HealthComponent(const ecs::Entity& entity) : HealthComponent(entity, 1) {};
+	HealthComponent(const ecs::Entity& entity) : HealthComponent(entity, 0) {};
 	HealthComponent(const ecs::Entity& entity, int health) : ecs::Component(entity), health(health) {};
 };
 
