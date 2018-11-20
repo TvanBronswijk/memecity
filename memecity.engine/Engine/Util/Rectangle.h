5d1b6aa9b57d13ed9dd0a2ada21d9676f174f742
Martijn van der Pol <martijn.pol@hotmail.com>
2018-11-20 10:41:33 +0100

    Collider component added

--- memecity.engine/Engine/Util/Rectangle.h
+++ memecity.engine/Engine/Util/Rectangle.h
@@ -1,5 +1,6 @@
 ﻿#ifndef _RECTANGLE_H
 #define _RECTANGLE_H
+
 struct Rectangle
 {
 	int x, y, w, h;
@@ -7,27 +8,6 @@ struct Rectangle
 		: x(x), y(y), w(w), h(h) {};
 	Rectangle() = default;
 	virtual ~Rectangle() = default;
-
-	bool contains(const Rectangle &other) const
-	{
-		return this->contains(other.x, other.y);
-	}
-
-	bool contains(int other_x, int other_y) const
-	{
-		return (other_x >= x - w &&
-			other_x <= x + w &&
-			other_y >= y - h &&
-			other_y <= y + h);
-	}
-
-	bool intersects(Rectangle &other) const
-	{
-		return !(other.x - other.w > x + w ||
-			other.x + other.w < x - w ||
-			other.y - other.h > y + h ||
-			other.y + other.h < x - h);
-	}
 };
 
 #endif
--- memecity.engine/QuadTree.h
+++ memecity.engine/QuadTree.h
@@ -1,29 +1,115 @@
 #ifndef _QUAD_TREE_H
 #define _QUAD_TREE_H
+
 #include <vector>
 #include <memory>
-#include "Engine/Textures/Texture.h"
+#include "Engine/Collider.h"
 
+template <class T>
 class QuadTree
 {
-public:
-	QuadTree(Rectangle &rectangle, size_t capacity) : is_divided(false), boundary(rectangle), capacity(capacity) {}
-	bool insert(const Rectangle& rectangle);
-	std::vector<Rectangle> query(Rectangle& range);
-
 private:
-	bool is_divided;
+	bool _is_divided;
 
-	std::unique_ptr<QuadTree> northwest;
-	std::unique_ptr<QuadTree> northeast;
-	std::unique_ptr<QuadTree> southwest;
-	std::unique_ptr<QuadTree> southeast;
+	std::unique_ptr<QuadTree> _northwest;
+	std::unique_ptr<QuadTree> _northeast;
+	std::unique_ptr<QuadTree> _southwest;
+	std::unique_ptr<QuadTree> _southeast;
 
-	std::vector<Rectangle> rectangles;
-	Rectangle boundary;
-	size_t capacity;
+	std::vector<T> _objects;
+	Collider _boundary;
+	std::size_t _capacity;
 
-	void subdivide();
+	void subdivide()
+	{
+		auto x = this->_boundary.x;
+		auto y = this->_boundary.y;
+		auto w = this->_boundary.w;
+		auto h = this->_boundary.h;
+
+		auto nw = Collider(x + (w / 2), y - (h / 2), (w / 2), (h / 2));
+		this->_northwest = std::make_unique<QuadTree>(nw, _capacity);
+
+		auto ne = Collider(x - (w / 2), y - (h / 2), (w / 2), (h / 2));
+		this->_northeast = std::make_unique<QuadTree>(ne, _capacity);
+
+		auto sw = Collider(x + (w / 2), y + (h / 2), (w / 2), (h / 2));
+		this->_southwest = std::make_unique<QuadTree>(sw, _capacity);
+
+		auto se = Collider(x - (w / 2), y + (h / 2), (w / 2), (h / 2));
+		this->_southeast = std::make_unique<QuadTree>(se, _capacity);
+
+		this->_is_divided = true;
+	}
+
+public:
+	QuadTree(T &object, const std::size_t capacity, const Collider &boundary) : _is_divided(false), _boundary(boundary), _capacity(capacity) {}
+	
+	bool insert(const T& object)
+	{
+		if (!this->_boundary.contains(object))
+		{
+			return false;
+		}
+
+		if (this->_objects.size() < this->_capacity)
+		{
+			this->_objects.push_back(object);
+			return true;
+		}
+
+		if (!this->_is_divided)
+		{
+			this->subdivide();
+		}
+
+		if (this->_northeast->insert(object))
+		{
+			return true;
+		}
+		if (this->_northwest->insert(object))
+		{
+			return true;
+		}
+		if (this->_southeast->insert(object))
+		{
+			return true;
+		}
+		if (this->_southwest->insert(object))
+		{
+			return true;
+		}
+
+		return false;
+	}
+
+	std::vector<Collider> query(T& range)
+	{
+		std::vector<T> found_objects;
+
+		if (!this->_boundary.intersects(range))
+		{
+			return found_objects;
+		}
+
+		for (const auto& object : _objects)
+		{
+			if (range.contains(object))
+			{
+				found_objects.push_back(object);
+			}
+		}
+
+		if (this->_is_divided)
+		{
+			found_objects.insert(found_objects.begin(), this->_northeast->query(range).begin(), this->_northeast->query(range).end());
+			found_objects.insert(found_objects.begin(), this->_northwest->query(range).begin(), this->_northwest->query(range).end());
+			found_objects.insert(found_objects.begin(), this->_southeast->query(range).begin(), this->_southeast->query(range).end());
+			found_objects.insert(found_objects.begin(), this->_southwest->query(range).begin(), this->_southwest->query(range).end());
+		}
+
+		return found_objects;
+	}
 };
 
 #endif
