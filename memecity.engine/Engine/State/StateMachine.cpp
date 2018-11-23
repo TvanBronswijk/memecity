52e9dfabf7e60c58caee2c8eb7f47ef64654544a
Tobi Bronswijk <294604@student.fontys.nl>
2018-11-23 06:45:48 +0100

    state switching semantics

--- memecity.engine/Engine/State/StateMachine.cpp
+++ memecity.engine/Engine/State/StateMachine.cpp
@@ -1,32 +1,56 @@
 #include "StateMachine.h"
+#include "..\Exceptions.h"
 #include "State.h"
 
 namespace memecity::engine::state {
-	void StateMachine::init() const {
-		current_state().on_load();
-		current_state().on_enter();
+
+	void StateMachine::exit() const
+	{
+		if (_stack.size() > 0) {
+			current_state()->on_exit();
+		}		
+	}
+
+	void StateMachine::enter() const
+	{
+		if (_stack.size() > 0) {
+			current_state()->on_enter();
+		}
+	}
+
+	void StateMachine::load() const
+	{
+		if (_stack.size() > 0) {
+			current_state()->on_load();
+		}
 	}
 
 	void StateMachine::pop(int items)
 	{
 		for (int i = 0; i < items; i++)
 		{
-			current_state().on_exit();
+			if (_stack.size() <= 0) {
+				throw exceptions::MemeException(exceptions::Level::error, "Statemachine Stack is empty while pop queue is not.");
+			}
+
+			exit();
 			_stack.pop();
 		}
-		current_state().on_enter();
+		enter();
 	}
 
-	State & StateMachine::current_state() const
+	State* StateMachine::current_state() const
 	{
-		return *(_stack.top());
+		if (_stack.size() > 0)
+			return _stack.top().get();
+		return nullptr;
 	}
 
 	void StateMachine::update(float dt) const {
-		current_state().update(dt);
+		current_state()->update(dt);
 	}
 
 	void StateMachine::draw() const {
-		current_state().draw();
+		current_state()->draw();
 	}
 }
--- memecity.engine/Engine/State/StateMachine.h
+++ memecity.engine/Engine/State/StateMachine.h
@@ -8,18 +8,23 @@ namespace memecity::engine::state {
 	class StateMachine {
 	private:
 		std::stack<std::unique_ptr<State>> _stack;
-		void init() const;
+		void exit() const;
+		void load() const;
+		void enter() const;
 	public:
 		StateMachine() = default;
 		template<class T, class ... Args>
 		void create_state(Args&& ... args) {
 			static_assert(std::is_convertible<T*, State*>::value, "This function can only construct concrete subclasses of State");
 			static_assert(std::is_constructible<T, StateMachine&, Args...>::value, "The requested type cannot be constructed from the arguments provided.");
+
+			exit();
 			_stack.push(std::make_unique<T>(*this, std::forward<Args>(args)...));
-			init();
+			load();
+			enter();
 		}
 		void pop(int items = 1);
-		State& current_state() const;
+		State* current_state() const;
 		void update(float dt) const;
 		void draw() const;
 	};
