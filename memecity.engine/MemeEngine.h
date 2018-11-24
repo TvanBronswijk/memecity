41c6c1f9277fe48c561ec45ebf918da1395f10d3
Tobi Bronswijk <294604@student.fontys.nl>
2018-11-24 15:52:58 +0100

    add documentation

--- memecity.engine/MemeEngine.h
+++ memecity.engine/MemeEngine.h
@@ -28,28 +28,27 @@ namespace memecity::engine {
 		MultimediaManager multimedia_manager{ false };
 		InputManager input_manager{};
 		sdl::TimerFacade timer;
+		///<summary>Initialization logic</summary>
 		virtual void init() = 0;
 	public:
 		MemeEngine() = default;
 		~MemeEngine() = default;
+		///<summary>Update game logic and handle user input</summary>
 		virtual void update(float dt) = 0;
+		///<summary>Render textures</summary>
 		virtual void draw() = 0;
+		///<summary>Start the game. Optional Threading flag allows you to choose between multithreaded and singlethreaded runner. Defaults to singlethreaded.</summary>
 		int run(Threading flag = Threading::singlethreaded);
+		///<summary>Start the game. Custom runner.</summary>
 		int run(Runnable<int> runnable);
+		///<summary>Set one of the default runners. To be used for runtime switching of runner.</summary>
 		virtual void set_runnable(Threading flag);
+		///<summary>Set to a custom runner. To be used for runtime switching of runner.</summary>
 		virtual void set_runnable(Runnable<int> runnable);
-		 MultimediaManager& get_multimedia_manager()
-		{
-			return multimedia_manager;
-		}
-		InputManager& get_input_manager()
-		{
-			return input_manager;
-		}
-		sdl::TimerFacade& get_timer()
-		{
-			return timer;
-		}
+
+		 MultimediaManager& get_multimedia_manager() { return multimedia_manager; }
+		InputManager& get_input_manager() { return input_manager; }
+		sdl::TimerFacade& get_timer() { return timer; }
 	};
 }
 
