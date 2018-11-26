5ee8fb763fe5bd1de6b7348f5f57f0d2c0f1e1dd
Martijn van der Pol <martijn.pol@hotmail.com>
2018-11-26 11:39:24 +0100

    CurrentState is set in the initialization list

--- memecity.game/Game/Components/AnimationComponent.h
+++ memecity.game/Game/Components/AnimationComponent.h
@@ -7,8 +7,8 @@ struct AnimationComponent : public memecity::engine::ecs::Component
 {
 	enum class AnimationState { idle, fighting };
 
-	AnimationState current_state = AnimationState::idle;
-	AnimationComponent(memecity::engine::ecs::Entity& entity) : memecity::engine::ecs::Component(entity){}
+	AnimationState current_state;
+	AnimationComponent(memecity::engine::ecs::Entity& entity) : memecity::engine::ecs::Component(entity), current_state(AnimationState::idle) {}
 };
 
 #endif
