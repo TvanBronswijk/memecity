28231f8b70a4c2b84b54b3069f90c76ce80a3d21
Tobi Bronswijk <294604@student.fontys.nl>
2018-11-26 11:26:52 +0100

    added loading bar

--- memecity.game/Game/States/LoadingState.h
+++ memecity.game/Game/States/LoadingState.h
@@ -2,16 +2,18 @@
 #define  _GAME_LOADING_STATE_H
 #include <functional>
 #include <Engine\State.h>
+#include <UI\Loading.h>
 #include "..\..\GameManager.h"
 
 struct LoadingState : public memecity::engine::state::State {
-	using Loader = std::function<void(GameManager::GameContext&)>;
+	using Loader = std::function<void(GameManager::GameContext&, memecity::engine::ui::loading::LoadingBar::Listener& listener)>;
 private:
 	GameManager::GameContext* _context;
-	Loader _loader;
+	memecity::engine::ui::loading::LoadingBar _loading_bar;
+	LoadingState::Loader _loader;
 public:
-	LoadingState(memecity::engine::state::StateManager& sm, GameManager::GameContext& gc, Loader loader)
-		: State(sm), _context(&gc), _loader(loader) {}
+	LoadingState(memecity::engine::state::StateManager& sm, GameManager::GameContext& gc, LoadingState::Loader loader)
+		: State(sm), _context(&gc), _loader(loader), _loading_bar(gc.get_multimedia_manager()) {}
 	~LoadingState() = default;
 	void on_load() override;
 	void update(float dt) override;
