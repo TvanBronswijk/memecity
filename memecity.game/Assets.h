c734099d44dd140c460eee407d136a88d34b937c
Tobi Bronswijk <294604@student.fontys.nl>
2018-11-27 21:53:33 +0100

    compile-time variables

--- memecity.game/Assets.h
+++ memecity.game/Assets.h
@@ -2,22 +2,23 @@
 #define _GAME_ASSETS_H
 
 namespace assets {
+	using Asset = const char*;
 	namespace fonts {
-		static const char* DEFAULT_FONT = "Fonts/Minecraftia-Regular.ttf";
+		static constexpr Asset DEFAULT_FONT = "Fonts/Minecraftia-Regular.ttf";
 	}
 	namespace music {
-		static const char* DEFAULT_BGM = "Music/bgm-game.mp3";
+		static constexpr Asset DEFAULT_BGM = "Music/bgm-game.mp3";
 	}
 	namespace sprites {
 		namespace tiles {
-			static const char* GRASS = "Sprites/Tiles/green.bmp";
-			static const char* ROAD = "Sprites/Tiles/gray.bmp";
-			static const char* WALL = "Sprites/Tiles/brown.bmp";
-			static const char* WATER = "Sprites/Tiles/blue.bmp";
+			static constexpr Asset GRASS = "Sprites/Tiles/green.bmp";
+			static constexpr Asset ROAD = "Sprites/Tiles/gray.bmp";
+			static constexpr Asset WALL = "Sprites/Tiles/brown.bmp";
+			static constexpr Asset WATER = "Sprites/Tiles/blue.bmp";
 		}
 	}
 	namespace spritesheets {
-		static const char* HUMAN_MALE_1 = "Spritesheets/SpriteSheet.png";
+		static constexpr Asset HUMAN_MALE_1 = "Spritesheets/SpriteSheet.png";
 	}
 };
 
