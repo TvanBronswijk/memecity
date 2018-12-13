e612719e837c3c8c084e1a806ef26711ba0652ac
Rick van Berlo <rickvanberlo@gmail.com>
2018-12-13 11:27:10 +0100

    changes npc position

--- memecity.game/Game/GameLoader.cpp
+++ memecity.game/Game/GameLoader.cpp
@@ -50,7 +50,7 @@ void GameLoader::create_npcs(EntityManager& em, loading::LoadingBar::Listener& l
 	for (size_t i = 0; i < 20; i++) {
 		auto& multimedia_manager = _context->get_multimedia_manager();
 		auto& timer = _context->get_timer();
-		generate::NPCGenerator(multimedia_manager, em).generate_random_npc(1, 220, 0, 1, State::Roaming);
+		generate::NPCGenerator(multimedia_manager, em).generate_random_npc(1, 50 * i, 0, 1, State::Roaming);
 	}
 	listener.increase_current_value(20.0f);
 }
