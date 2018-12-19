39bea77042b489d5e0ab603f2bd981558cf07485
Yoeri <y.vanhoof@student.avans.nl>
2018-12-19 20:38:43 +0100

    another merge

--- memecity.game/Game/LevelBuilder.h
+++ memecity.game/Game/LevelBuilder.h
@@ -15,7 +15,7 @@ private:
 
 public:
 	LevelBuilder(GameManager::GameContext& context, int level, int map_width, int map_height, bool load_from_file, std::string save_location = "")
-		: _context(&context), level(level), _map_width(map_width), _map_height(map_height), _load_from_file(load_from_file),_save_location(save_location) {};
+		: _context(&context), _level(level), _map_width(map_width), _map_height(map_height), _load_from_file(load_from_file),_save_location(save_location) {};
 	~LevelBuilder() = default;
 
 	Point build(memecity::engine::ecs::EntityManager& em, memecity::engine::ui::loading::LoadingBar::Listener& listener, int& map_number);
