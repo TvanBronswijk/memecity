30ba72c496e8ff302cec4b12a22753d099b751a2
Tobi Bronswijk <294604@student.fontys.nl>
2018-12-19 14:55:51 +0100

    proper object layer loading

--- memecity.game/Game/Generate/City/CityGenerator.cpp
+++ memecity.game/Game/Generate/City/CityGenerator.cpp
@@ -8,12 +8,16 @@ generate::CityGenerator::CityGenerator(memecity::engine::StorageManager& storage
 
 	auto prefab_dir = storage.load_files_from_directory("Assets/Scripts/Prefabs");
 	for (auto& pair : prefab_dir) {
-		auto str = pair.second;
-		str.erase(std::remove(str.begin(), str.end(), '\n'), str.end());
-		int w, h;
-		w = 16;
-		h = 16;
-		_prefabs.push_back(models::Prefab{ w, h, str.c_str(), " " });
+		if (pair.first.path().extension() == ".m2d") {
+			auto other_pair = std::find_if(prefab_dir.begin(), prefab_dir.end(), [&](auto& other_pair) 
+			{ return other_pair.first.path().stem() == pair.first.path().stem() && other_pair.first.path().extension() == ".m2do"; });
+			
+			auto content = pair.second;
+			content.erase(std::remove(content.begin(), content.end(), '\n'), content.end());
+			auto objects = other_pair->second;
+			objects.erase(std::remove(objects.begin(), objects.end(), '\n'), objects.end());
+			_prefabs.push_back(models::Prefab{ 16, 16, content.c_str(), objects.c_str() });
+		}
 	}
 
 	for (auto& prefab : _prefabs) {
